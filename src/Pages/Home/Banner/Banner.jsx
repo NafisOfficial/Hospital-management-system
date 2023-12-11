405df76d06444064fb4974478096262219b9bec2
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2023-12-11 23:44:11 +0600

    project from Izharul

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import banner1 from '../../../assets/Photos/banner/banner2.png'
 import { IoCall } from "react-icons/io5";
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
@@ -11,7 +12,7 @@ const Banner = () => {
                     <div className='px-12'>
                         <h1 className="text-3xl font-bold">Emergency Ambulance Service!</h1>
                         <p className="py-6">We provide 24/7 hours emergency ambulance services ! If it is urgent to you, you can't take our ambulance services.</p>
-                        <button className="btn btn-primary"><IoCall/> Call Ambulance</button>
+                        <button className="btn btn-primary"><Link to='tel:+8801701234567'>Call Ambulance</Link></button>
                     </div>
                 </div>
             </div>

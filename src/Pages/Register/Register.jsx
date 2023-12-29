dc99ea341cb798717ff39e4a8ee3934765472c8a
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2023-12-29 20:50:22 +0600

    registration form customization complete

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,4 +1,6 @@
 import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebookSquare } from "react-icons/fa";
 import bg from "../../assets/Photos/Login/background1.jpg";
 import { Form, Link } from "react-router-dom";
 
@@ -12,6 +14,13 @@ const Register = () => {
   const onSubmit = ({ name, email, newPassword, confirmPassword }) => {
     console.log(name, email, newPassword, confirmPassword);
   };
+  
+  const signInWithGoogle=(event)=>{
+    event.preventDefault();
+  }
+  const signInWithFacebook=(event)=>{
+    event.preventDefault();
+  }
 
   return (
     <div
@@ -23,16 +32,14 @@ const Register = () => {
       <div className="hero-overlay bg-opacity-70"></div>
       <div>
         <div className="border rounded-lg px-12">
-          <div className="text-4xl font-bold text-white text-center mt-10">
+          <div className="text-3xl font-bold text-white text-center mt-8">
             Register Now
           </div>
           <Form className="card-body" onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control gap-1">
               <div className="form-control">
                 <label className="label pb-0 px-0">
-                  <span className="text-white">
-                    Enter your name
-                  </span>
+                  <span className="text-white">Enter your name</span>
                 </label>
                 <input
                   type="text"
@@ -44,9 +51,7 @@ const Register = () => {
               </div>
               <div className="form-control">
                 <label className="label pb-0 px-0">
-                  <span className="text-white">
-                    Enter your email
-                  </span>
+                  <span className="text-white">Enter your email</span>
                 </label>
                 <input
                   type="email"
@@ -62,9 +67,7 @@ const Register = () => {
             <div className="form-control ">
               <div className="form-control">
                 <label className="label pb-0 px-0">
-                  <span className="text-white">
-                    New Password
-                  </span>
+                  <span className="text-white">New Password</span>
                 </label>
                 <input
                   type="password"
@@ -81,9 +84,7 @@ const Register = () => {
               <p className="py-1 text-sm">{errors.newPassword?.message}</p>
               <div className="form-control">
                 <label className="label pb-0 px-0">
-                  <span className="text-white">
-                    Confirm Password
-                  </span>
+                  <span className="text-white">Confirm Password</span>
                 </label>
                 <input
                   type="password"
@@ -96,10 +97,21 @@ const Register = () => {
               </div>
               <p className="py-1 text-sm">{errors.confirmPassword?.message}</p>
             </div>
-            <div className="form-control my-4">
-              <button className="btn btn-sm bg-[#3B82F6] border-0 hover:bg-transparent text-white">
+            <div className="form-control my-2">
+              <button className="btn bg-[#3B82F6] border-0 hover:bg-transparent text-white">
                 Register
               </button>
+              <div className="mt-2">
+                <p className="text-white">Try another way</p>
+                <div className="flex justify-between">
+                  <button onClick={signInWithGoogle} className="btn bg-transparent border-0 hover:bg-transparent text-white">
+                    <FcGoogle className="text-4xl"></FcGoogle>
+                  </button>
+                  <button onClick={signInWithFacebook} className="btn bg-transparent border-0 hover:bg-transparent text-white">
+                    <FaFacebookSquare className="text-4xl" />
+                  </button>
+                </div>
+              </div>
             </div>
             <div className="form-control text-white">
               If you already have an account ? please
